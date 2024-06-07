b8c8e596c560466a8eeabe98ffbdeb67813c02e6
Putu Krisna Andyartha <putukrisna6@gmail.com>
2024-06-07 22:32:23 +0700

    fix(alembic): simplify versioning + autoincrement

--- app/db/models.py
+++ app/db/models.py
@@ -4,5 +4,5 @@ from app.db.base import Base
 
 class Feedback(Base):
     __tablename__ = 'feedbacks'
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     score = Column(Integer, CheckConstraint('score >= 1 AND score <= 5'), nullable=False)
